3012d838e7c3665bb4e815532f99e44f793c998a
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-12-06 00:15:48 -0800

    Implemented Dark mode option

---
+++ context/ColorModeContext.js
@@ -0,0 +1,38 @@
+import { useMemo, useState, useContext, createContext } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+  mode: "light",
+});
+
+export const ColorModeContextProvider = ({ children }) => {
+  const [mode, setMode] = useState("light");
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+      mode,
+    }),
+    [mode]
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
+
+export const useColorMode = () => useContext(ColorModeContext);
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import React from "react";
+import { ColorModeContextProvider } from "../context/ColorModeContext";
+import { NavBar } from "../components";
+import { Box } from "@mui/system";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ColorModeContextProvider>
+      <Box
+        sx={{
+          width: "100vw",
+          height: "100vh",
+          backgroundColor: "background.default",
+        }}
+      >
+        <NavBar />
+        <Component {...pageProps} />
+      </Box>
+    </ColorModeContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
