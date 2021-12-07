90b1595c83cb4b6682cc762fda61cd4ad692ce61
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-12-07 02:24:10 -0800

    updated landing page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { Container, Paper, Typography, Button } from "@mui/material";
+import { Container, Paper, Typography, Button, Stack } from "@mui/material";
 import { useRouter } from "next/router";
 import { MetaTags } from "../components";
+import Image from "next/image";
 
 const Home = () => {
   const router = useRouter();
@@ -9,10 +10,34 @@ const Home = () => {
     <>
       <MetaTags title="Home" />
       <Container align="center" maxWidth="xl">
-        <Paper elevation={5} sx={{ my: 3, py: 3 }}>
-          <Typography align="center" variant="h1">
-            <u>soMLiere</u>
-          </Typography>
+        <Paper elevation={5} sx={{ my: 3, py: 7 }}>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Stack direction="row">
+              <Typography align="center" variant="h2">
+                <u>soMLiere</u>
+              </Typography>
+              <Image
+                src="/logo-soMLiere.png"
+                alt="logo"
+                width="80"
+                height="50"
+              />
+            </Stack>
+          </div>
+          <Container maxWidth="sm">
+            <Typography variant="h6" sx={{ py: 5 }}>
+              Wine experts usually differentiated wines according to its smell,
+              flavor, and color. But what if you are not a wine expert or a{" "}
+              <b>Sommelier</b> who specialize in all aspects of wine service as
+              well as wine and food paring?
+            </Typography>
+            <Typography variant="h6">
+              Well look no further, <b>soMLiere</b> (play on words on Sommelier)
+              uses Machine Learning to check the quality of your wine. No need
+              to spend your whole life acquiring a refined pallette just to rate
+              some old grape juice.
+            </Typography>
+          </Container>
         </Paper>
         <Button
           onClick={(e) => {
@@ -20,7 +45,7 @@ const Home = () => {
             router.push("/quality");
           }}
         >
-          Continue
+          Get your wine rated
         </Button>
       </Container>
     </>
