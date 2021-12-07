ea9f700239dd84634c0738995c192e96ee93fca8
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-12-07 02:24:33 -0800

    updated root

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,14 @@ import "../styles/globals.css";
 import React from "react";
 import { ColorModeContextProvider } from "../context/ColorModeContext";
 import { NavBar } from "../components";
-import { Box } from "@mui/system";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ColorModeContextProvider>
-      <Box
-        sx={{
-          width: "100vw",
-          height: "100vh",
-          backgroundColor: "background.default",
-        }}
-      >
-        <NavBar />
-        <Component {...pageProps} />
-      </Box>
+      <CssBaseline />
+      <NavBar />
+      <Component {...pageProps} />
     </ColorModeContextProvider>
   );
 }
